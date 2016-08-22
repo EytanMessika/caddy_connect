a301b32183ba459024b543e9943653a008608d28
Manou <mfebvret@gmail.com>
2016-08-22 21:28:00 +0200

    product index view and style

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all 
+  end 
+
+  def create
+  end 
+
+  def new
+  end 
+
+  def update
+  end 
+
+  def edit
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :products , dependent: :destroy
+  # :nullify for later, change status dependent
 end
