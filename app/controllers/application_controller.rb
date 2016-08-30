bbb8285163b4cf25f538de817a29999ee5e1102c
Mael Singh <maelsingh@gmail.com>
2016-08-30 16:02:50 +0200

    config app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  before_action :set_unread_notifications, if: :user_signed_in?
+
+  private
+
+  def set_unread_notifications
+    @notifications = PublicActivity::Activity.where(recipient: current_user, read_at: nil)
+  end
+
 end
