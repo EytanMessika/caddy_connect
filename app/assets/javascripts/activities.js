635a337a2ab6e5fd225f03901a18ac9c90b43fec
Mael Singh <maelsingh@gmail.com>
2016-08-31 12:31:01 +0200

    notif done

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,11 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function() {
-  $(".read-activity").on("click", function() {
-    var id = $(this).data("id");
-    $.ajax({
-      type: "PATCH",
-      url: "/activities/" + id + ".js"
-    });
-  });
-});
--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,8 +1,13 @@
 class ActivitiesController < ApplicationController
 
   def update
-    PublicActivity::Activity.where(id: params[:id])
-
+    @activity = PublicActivity::Activity.find(params[:id])
+    @activity.read_at = Date.today
+    @activity.save
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 
 end
