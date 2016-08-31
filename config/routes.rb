4ad3869a9e5ab26ea81f3564116ee9e7d16b44f7
Paul HERRENSCHMIDT <herrenschmidtpaul@gmail.com>
2016-08-31 11:47:20 +0200

    conflict resolve

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,12 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :payments, only: [:create]
   resources :products, only: [:update]
+  authenticated :user do
+   root 'products#index', as: :authenticated_root
+  end
   get "/stats", to: "products#stats"
   get "stats/graph", to: "products#graph"
   get "/premium", to: "payments#premium", as: :premium
   root "pages#home"
   post "/webhooks" => "webhooks#delivery_status"
-     authenticated :user do
-       root 'products#index', as: :authenticated_root
-     end
-     get "/stats", to: "products#stats"
-     # get "stats/graph", to: "products#graph"
-     root "pages#home"
-     post "/webhooks" => "webhooks#delivery_status"
 end
