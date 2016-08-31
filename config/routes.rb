b5ebe78f0f87cb086389ab7ff5f7ce1f11b9a3b0
Paul HERRENSCHMIDT <herrenschmidtpaul@gmail.com>
2016-08-31 11:39:44 +0200

    cleaning master

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :payments, only: [:create]
   resources :products, only: [:update]
-  authenticated :user do
-   root 'products#index', as: :authenticated_root
-  end
   get "/stats", to: "products#stats"
   get "stats/graph", to: "products#graph"
   get "/premium", to: "payments#premium", as: :premium
